04e472b742c99a7f355ddb078d2a5f996f192370
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-06-09 10:08:22 -0500

    Rich text styles

--- themes/hcada/inc/js/base.js
+++ themes/hcada/inc/js/base.js
@@ -1,5 +1,22 @@
 (function($) {
 
+  // Anchor Scroll
+  $(function() {
+    $('a[href*=#]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
   //Equal Height Divs
   $(window).load(function() {
     var copy_height = $('.aspot .copy-container, .quote-with-image .quote-container').outerHeight();
--- themes/hcada/inc/partials/widgets/rich-text.php
+++ themes/hcada/inc/partials/widgets/rich-text.php
@@ -1,8 +1,9 @@
 <?php
 $content = get_sub_field('content');
 $section_id = get_sub_field('section_id');
+$color = get_sub_field('color');
 ?>
-<div <?php if ($section_id) { echo 'id="' . $section_id . '"'; } ?> class="inner">
+<div <?php if ($section_id) { echo 'id="' . $section_id . '"'; } ?> class="inner <?php echo $color; ?>">
   <div class="wrap">
     <div class="content content-box">
       <div class="inner">
