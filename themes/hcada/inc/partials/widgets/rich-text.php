d85689f9cc008ddc7433946b0af60b5f35d27c81
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-06-13 21:42:21 -0500

    Colors

--- themes/hcada/inc/partials/widgets/rich-text.php
+++ themes/hcada/inc/partials/widgets/rich-text.php
@@ -3,8 +3,12 @@ $content = get_sub_field('content');
 $section_id = get_sub_field('section_id');
 $color = get_sub_field('color');
 $alt_box = get_sub_field('alt_box');
+$background_image = get_sub_field('background_image')['url'];
 ?>
-<div <?php if ($section_id) { echo 'id="' . $section_id . '"'; } ?> class="inner <?php echo $color; ?>">
+<div class="inner <?php echo $color; if ($background_image) { echo ' bg-image'; } ?>">
+  <?php if ($background_image) { ?>
+    <div class="floating-background" style="background-image: url(<?php echo $background_image; ?>)"></div>
+  <?php } ?>
   <div class="wrap">
     <div class="content content-box<?php if ($alt_box) { echo ' box-alt'; } ?>">
       <div class="inner">
