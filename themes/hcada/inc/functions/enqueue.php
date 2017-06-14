75a91c46a49773d456e201a6976cf7b4a4a39e45
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-06-14 07:48:01 -0500

    Events start

--- themes/hcada/inc/functions/enqueue.php
+++ themes/hcada/inc/functions/enqueue.php
@@ -11,6 +11,7 @@ function hcada_load_scripts() {
   wp_enqueue_script( 'scrollmagic', get_template_directory_uri() . '/inc/js/libs/scrollmagic/ScrollMagic.js', null, null, true );
   wp_enqueue_script( 'animation', get_template_directory_uri() . '/inc/js/libs/scrollmagic/animation.gsap.js', null, null, true );
   wp_enqueue_script( 'indicators', get_template_directory_uri() . '/inc/js/libs/scrollmagic/debug.addIndicators.js', null, null, true );
+  wp_enqueue_script( 'slick', get_template_directory_uri() . '/inc/js/libs/slick.js', null, null, true );
   wp_enqueue_script( 'scroll', get_template_directory_uri() . '/inc/js/scroll.js', null, null, true );
   wp_enqueue_script( 'map-stuff', get_template_directory_uri() . '/inc/js/map-stuff.js', null, null, true );
   wp_enqueue_script( 'base', get_template_directory_uri() . '/inc/js/base.js', null, null, true );
--- themes/hcada/inc/js/base.js
+++ themes/hcada/inc/js/base.js
@@ -4,7 +4,6 @@
   $(function() {
     $('a[href*=#]').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
@@ -18,6 +17,15 @@
   });
 
 
+  // Activates the menu toggle
+  $('.menu-toggle').click(function() {
+    $('.business-nav').addClass('open')
+  })
+
+  $('.close-menu').click(function() {
+    $('.business-nav').removeClass('open')
+  })
+
   // Equal Heights Children - Cnt needs height
   function equalHeight(cnt) {
       var elementPadding = $(cnt).children().css('padding-top').slice(0,-2)*1 + $(cnt).children().css('padding-bottom').slice(0,-2)*1;
@@ -29,18 +37,17 @@
       minHeight = minHeight + elementPadding;
       $(cnt).children().not('.button').css('height', minHeight);
   }
-  if ($('.equal-height').children().length) {
+  if ($('.equal-height, .events-list .slick-track').children().length) {
     setTimeout(function() {
-      $('.equal-height').each(function() { equalHeight(this); });
+      $('.equal-height, .events-list .slick-track').each(function() { equalHeight(this); });
     }, 100);
 
     $(window).resize(function() {
-      $('.equal-height').each(function() {
+      $('.equal-height, .events-list .slick-track').each(function() {
         equalHeight(this);
       });
     })
   }
-
 
   //Sticky Header Classes
   function popOver() {
@@ -74,11 +81,6 @@
     }
   });
 
-  // Activates the menu toggle
-  $('#menu-toggle').click(function() {
-    $('html').toggleClass('slideOver');
-  });
-
   // Actives menu scrolling class
   $(window).bind('scroll', function() {
     if ($(window).scrollTop()) {
@@ -90,64 +92,32 @@
     }
   });
 
-  //Resets the menu
-  $(window).resize(function() {
-    $('html').removeClass('slideOver');
-  });
 
-  //Animated Counter
-  $('.count').each(function () {
-      $(this).prop('Counter',0).animate({
-          Counter: $(this).text()
-      }, {
-          duration: 2000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now));
-          }
-      });
-  });
-
-  //Removes any epmty <p>
-  $('.copy-container p').each(function() {
-    var $this = $(this);
-    if ($this.html().replace(/\s|&nbsp;/g, '').length === 0)
-      $this.remove();
-  });
-
-  //Resizes the "Logo Row" images to half size
-  $(window).load(function() {
-    $('.logos img').each(function () {
-      var w = $(this).outerWidth();
-      $(this).attr('width', w/2);
-    });
-
-    if (document.body.scrollTop === 0) {
-      $('header').addClass('top');
-    } else {
-      $('header').removeClass('top');
+$('.events-list').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2.2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1.1,
+        slidesToScroll: 1
+      }
     }
-
-  });
-
-  // Activates the menu toggle
-  $('#menu-toggle').click(function() {
-    $('body').toggleClass('slideOver');
-  });
-  //Resets the menu
-  $(window).resize(function() {
-    $('body').removeClass('slideOver');
-  });
-
-
-  $('.menu-toggle').click(function() {
-    $('.business-nav').addClass('open')
-  })
-
-  $('.close-menu').click(function() {
-    $('.business-nav').removeClass('open')
-  })
-
-
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 }(jQuery));
