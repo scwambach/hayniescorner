dca011d2b68e45dd8418d67d3e3d1fae6cdf6a42
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-06-22 07:53:43 -0500

    Single business styles

--- themes/hcada/single-business.php
+++ themes/hcada/single-business.php
@@ -5,12 +5,19 @@ $urlImage = $urlImage[0];
 $quote = get_field('quote');
 $copy_position = get_field('copy_position');
 $quote_position = get_field('quote_position');
+$image_position = get_field('image_position');
 get_header();
 if ( have_posts() ) :
 while ( have_posts() ) :
 the_post();
 ?>
-<div class="inner-business" style="background-image: url(<?php echo $urlImage; ?>)">
+<div class="inner-business <?php echo $image_position; ?>" style="background-image: url(<?php echo $urlImage; ?>)">
+  <div class="featured-image <?php echo $image_position; ?>" style="background-image: url(<?php echo $urlImage; ?>)"></div>
+  <div class="label">
+    <div class="inner">
+      <h1><?php the_title(); ?></h1>
+    </div>
+  </div>
   <div class="copy-block <?php echo $copy_position; ?>">
     <div class="inner">
       <?php the_content(); ?>
@@ -19,15 +26,10 @@ the_post();
   <?php if ($quote) { ?>
   <div class="quote-block <?php echo $quote_position; ?>">
     <div class="inner">
-      <blockquote><?php echo $quote; ?></blockquote>
+      <blockquote><span class="lq">&ldquo;</span><?php echo $quote; ?><span class="rq">&rdquo;</span></blockquote>
     </div>
   </div>
   <? } ?>
-</div>
-<div class="label">
-  <div class="inner">
-    <h1><?php the_title(); ?></h1>
-  </div>
 </div>
 <?php
 endwhile;
