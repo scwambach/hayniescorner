17adb2485c7710a03cd8cd698ee8c8ff1c17b59d
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-07-16 16:57:14 -0500

    Testing

--- themes/hcada/front-page.php
+++ themes/hcada/front-page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<!-- test -->
+<!-- testing -->
 <?php get_template_part('/inc/partials/widgets/aspot'); ?>
 <?php get_template_part('/inc/partials/widget', 'loop'); ?>
 <?php get_footer(); ?>
