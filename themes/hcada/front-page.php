80e4ada50985b6f07a704e90448fc965c89fada4
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-07-16 16:54:15 -0500

    Import and test

--- themes/hcada/front-page.php
+++ themes/hcada/front-page.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<!-- test -->
 <?php get_template_part('/inc/partials/widgets/aspot'); ?>
 <?php get_template_part('/inc/partials/widget', 'loop'); ?>
 <?php get_footer(); ?>
