cd8bc21393ba65dc368a66d3fc2138bf0575b522
Scott Wambach <scott.wambach@redstitchdigital.com>
2017-08-12 09:03:23 -0500

    Remove confirm

--- front-page.php
+++ front-page.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-<!-- confirm -->
 <?php get_template_part('/inc/partials/widgets/aspot'); ?>
 <?php get_template_part('/inc/partials/widget', 'loop'); ?>
 <?php get_footer(); ?>
